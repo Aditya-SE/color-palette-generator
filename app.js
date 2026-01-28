9f9ad8e07f8bab00ea9e8d729835e75c6ce0367a
Aditya-SE <adityapatil849703801313@gmail.com>
2026-01-28 08:28:05 +0530

    updated js file

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const generateBtn = document.getElementById("Generate-btn");
 const paletteContainer = document.querySelector(".palette-container");
 
+generateBtn.addEventListener("click",generatePalette);
+
+function generatePalette(){
+    const color = [];
+    for(let i = 0; i < 5 ; i++){
+        color.push(generatePalette());
+    }
+    
+}
+
+function generateRandomColor(){
+    const letters = "123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+
+    }
+    return color;
+}
+
+generatePalette();
