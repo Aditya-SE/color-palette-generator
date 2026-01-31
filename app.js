899f6ece76ff5083c7d3e66cb6d070c9e4ae74ca
Aditya-SE <adityapatil849703801313@gmail.com>
2026-01-31 17:04:46 +0530

    changing colors function added

--- app.js
+++ app.js
@@ -4,21 +4,33 @@ const paletteContainer = document.querySelector(".palette-container");
 generateBtn.addEventListener("click",generatePalette);
 
 function generatePalette(){
-    const color = [];
+    const colors = [];
     for(let i = 0; i < 5 ; i++){
-        color.push(generatePalette());
+        colors.push(generateRandomColor());
     }
-    
+    updatePaletteDisplay(colors);
 }
 
 function generateRandomColor(){
-    const letters = "123456789ABCDEF";
+    const letters = "0123456789ABCDEF";
     let color = "#";
     for(let i = 0; i < 6; i++){
         color += letters[Math.floor(Math.random()*16)];
 
     }
     return color;
+}
+
+function updatePaletteDisplay(colors){
+    const colorBoxes = document.querySelectorAll(".color-box");
+    colorBoxes.forEach((box , index)=>{
+        const color = colors[index];
+        const colorDiv = box.querySelector(".color");
+        const hexValue = box.querySelector(".hex-value");
+
+        colorDiv.style.backgroundColor = color;
+        hexValue.textContent = color;
+    });
 }
 
 generatePalette();
